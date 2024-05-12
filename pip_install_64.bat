a3c38e99bfe72c82a20891f3d6400c7e5ece08fc
youse <youseonho@gmail.com>
2024-05-12 23:02:51 +0900

    init commit

--- pip_install_64.bat
+++ pip_install_64.bat
@@ -21,8 +21,8 @@ if '%errorlevel%' NEQ '0' (
     CD /D "%~dp0"
     python64 -m pip install --upgrade pip
     python64 -m pip install pandas==2.0.3 websockets==10.4 cryptography==36.0.0 python-telegram-bot==13.15
-    python64 -m pip install numpy psutil pyqt5 PyQtWebEngine BeautifulSoup4 pyttsx3
-    python64 -m pip install lxml squarify matplotlib pyqtgraph pyupbit ntplib python-dateutil python-binance pyzmq
+    python64 -m pip install numpy psutil pyqt5 PyQtWebEngine BeautifulSoup4 pyttsx3 pyzmq
+    python64 -m pip install lxml squarify matplotlib pyqtgraph pyupbit ntplib python-dateutil python-binance
     python64 -m pip install numba
     python64 -m pip install ./utility/TA_Lib-0.4.25-cp311-cp311-win_amd64.whl
     pause
